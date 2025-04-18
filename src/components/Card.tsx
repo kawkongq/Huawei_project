54568f27a578358aefe9a32d2bd926eadcb2c471
Assari Yumyuang <a-s-rock12@hotmail.com>
2025-04-18 18:30:50 +0700

    add divinformation , selected province

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,16 +4,111 @@ import Image from 'next/image';
 export default function Productcard() {
     return (
         <div className={styles.card}>
-            <div className={styles.cardimg}>
-                <Image src={'/img/card1.jpg'}
-                alt='Product Picture'
-                fill = {true}
-                objectFit='cover'
-                />
+            <div className={styles.seconddiv}>
+                <div className={styles.ribbon}>TOP 1</div>
+                <div className={styles.imageWrapper}>
+                    <Image
+                    src="/img/card1.jpg"
+                    alt="Product Picture"
+                    fill
+                    objectFit="cover"
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h3 className={styles.title}>Hong Kong Disneyland</h3>
+                    <div className={styles.meta}>
+                    <span className={styles.rating}>
+                    4.7<span className={styles.outOf}>/5</span>
+                    </span>
+                    <span className={styles.reviews}>72,414 reviews</span>
+                    </div>
+                    
+                </div>
             </div>
-            <div className={styles.cardtext}>
-            A vaccine is a biological preparation that provides active acquired immunity to a particular infectious or malignant disease
+             
+
+         
+            <div className={styles.seconddiv}>
+                <div className={styles.ribbon}>TOP 2</div>
+                <div className={styles.imageWrapper}>
+                    <Image
+                    src="/img/card1.jpg"
+                    alt="Product Picture"
+                    fill
+                    objectFit="cover"
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h3 className={styles.title}>Hong Kong Disneyland</h3>
+                    <div className={styles.meta}>
+                    <span className={styles.rating}>
+                    4.7<span className={styles.outOf}>/5</span>
+                    </span>
+                    <span className={styles.reviews}>72,414 reviews</span>
+                    </div>
+                    
+                </div>
             </div>
+           
+
+            <div className={styles.seconddiv}>
+                <div className={styles.ribbon}>TOP 3</div>
+                <div className={styles.imageWrapper}>
+                    <Image
+                    src="/img/card1.jpg"
+                    alt="Product Picture"
+                    fill
+                    objectFit="cover"
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h3 className={styles.title}>Hong Kong Disneyland</h3>
+                    <div className={styles.meta}>
+                    <span className={styles.rating}>
+                    4.7<span className={styles.outOf}>/5</span>
+                    </span>
+                    <span className={styles.reviews}>72,414 reviews</span>
+                    </div>
+                    
+                </div>
+            </div>
+        
+        
+            {/* <div style={{width: "100%",
+                        height: "30%",
+                        marginTop: "9px",
+                        position: "relative"}}>                
+                            <Image src={'/img/card1.jpg'} 
+                            alt="Product Picture"
+                            fill = {true} 
+                            objectFit='cover'/>
+            </div>
+            <div style={{width: "100%",
+                        height: "30%",
+                        marginTop: "9px",
+                        position: "relative"}}>                
+                            <Image src={'/img/card1.jpg'} 
+                            alt="Product Picture"
+                            fill = {true} 
+                            objectFit='cover'/>
+            </div>
+            <div style={{width: "100%",
+                        height: "31%",
+                        marginTop: "9px",
+                        position: "relative"}}>                
+                            <Image src={'/img/card1.jpg'} 
+                            alt="Product Picture"
+                            fill = {true} 
+                            objectFit='cover'/>
+            </div> */}
+            {/* <div style={{width: "100%",
+                        height: "35%",
+                        position: "relative",               
+                        backgroundColor: "black",
+                        marginTop: "8px",
+                        marginBottom: "8px"}}>
+                        
+            </div> */}
         </div>
     )
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { ZoomableGroup } from "react-simple-maps";
-
+import Productcard from "./Card";
+import Image from 'next/image';
 // TopoJSON file for Thailand map
 const THAILAND_TOPO_JSON = "/thailand-provinces.topojson";
 
 const Map: React.FC = () => {
+    const [selectedProvince, setSelectedProvince] = useState<string>("");
+    const lastClicked = useRef<SVGElement | null>(null);
     const handleProvinceClick = (provinceName: string) => {
         console.log(`Clicked on province: ${provinceName}`);
         // Add your logic to connect to another component here
+        setSelectedProvince(provinceName);
+        
     };
     const MAP_WIDTH = 60;
     const [position, setPosition] = React.useState<{ coordinates: [number, number]; zoom: number }>({ coordinates: [100.9925, 15.8700], zoom: 1 });
@@ -36,7 +41,15 @@ const Map: React.FC = () => {
                                 <Geography
                                     key={geo.rsmKey}
                                     geography={geo}
-                                    onClick={() => handleProvinceClick(geo.properties.name)}
+                                    onClick={e => {
+                                        handleProvinceClick(geo.properties.NAME_1)
+                                    const el = e.currentTarget;
+                                    if (lastClicked.current) {
+                                        lastClicked.current.style.fill = "#FFFFFF";
+                                    }
+                                    el.style.fill = "#F53";
+                                    lastClicked.current = el;
+                                    }}
                                     style={{
                                         default: { fill: "#FFFFFF", stroke: "#D9D9D9", strokeWidth: 0.01 },
                                         hover: { fill: "#F53", stroke: "none" },
@@ -50,7 +63,24 @@ const Map: React.FC = () => {
                     </Geographies>
                 </ZoomableGroup>
             </ComposableMap>
+            {selectedProvince && (
+        <div style={{
+            width: "30%",
+            height: "95%",
+            padding: "0.5rem",
+            background: "#fff",
+            color: "#fff",
+            borderRadius: "15px",
+            margin: "1rem",
+
+          }}>
+          <h1 style={{textAlign: 'center',fontSize: '45px',color: 'black'}}>{selectedProvince}</h1>
+          
+          <Productcard/>
         </div>
+    )}
+        </div>
+       
     );
 };
 
